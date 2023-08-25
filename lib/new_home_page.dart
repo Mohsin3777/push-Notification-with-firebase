79531e8dde7a1723ecca75606b4ebf62a0d7d561
mohsin222 <mohsinirfan222@gmail.com>
2023-08-25 13:01:49 -0700

    basic series of Asif Taj Done

--- lib/new_home_page.dart
+++ lib/new_home_page.dart
@@ -1,6 +1,9 @@
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:pushh_notifcation/new_notification_class/firebase_class.dart';
+import 'package:http/http.dart' as http;
 
 class NEWHOMEPAGE extends StatefulWidget {
   const NEWHOMEPAGE({super.key});
@@ -29,11 +32,36 @@ firebaseNewClass.setupInteractMessage(context);
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      backgroundColor: Colors.pinkAccent,
+      backgroundColor: Colors.white,
       appBar: AppBar(title: Text('NEW CLASS PRACTICE'),),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-
+Center(
+  child: TextButton(child: Text('PRESS'),onPressed: (){
+firebaseNewClass.getDeviceToken().then((value)async {
+  var data ={
+    'to':value.toString(),
+    'priority': 'high',
+    'notification':{
+      'title':'Asif',
+      'body':'Subscribe to my channel'
+    },
+    'data':{
+        'chat': 'message',
+      'id': '1234567'
+    }
+  };
+await http.post(Uri.parse('https://fcm.googleapis.com/fcm/send'),
+body: jsonEncode(data),
+headers: {
+  'Content-Type': 'application/json; charset=UTF-8',
+  'Authorization' :'key=AAAAbP61VcY:APA91bHJL3pj8MeTkAXmHHb342o9wWH77JfWqSxYyNxq_VFz9sgvdpx5o7vxzXCSwVk21TEwFwqakYV4GNO5LqfzWYKgBbKYYdUqclo9fM6ibKHJb8ZOTeZ49cFzdDB4eqHQcgib1KUP'
+}
+);
+});
+  },),
+)
         ],
       ),
     );
